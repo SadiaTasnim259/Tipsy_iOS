a7aa30e869e5d3fe9627f2a39a959892c62a8ad5
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-01 19:57:40 +0600

    Design Complete

---
+++ Tipsy/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Sadia on 1/8/23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
