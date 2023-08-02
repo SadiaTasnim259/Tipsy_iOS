1f1fd9ac939b288bc1a0f19242b345970bd7aa96
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-02 16:44:23 +0600

    UI updated

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -17,18 +17,47 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     
     @IBAction func tipPersentageButton(_ sender: UIButton) {
+        inputTextField.endEditing(true)
+        resetAllTipButton()
+        
+        sender.configuration?.background.backgroundColor = UIColor.systemGreen
+        sender.configuration?.baseForegroundColor = UIColor.white
+        
+        var selectedTipPercentage = sender.titleLabel?.text
+        
+    }
+    
+    func resetAllTipButton(){
+        tenButton.configuration?.background.backgroundColor = UIColor.clear
+        tenButton.configuration?.baseForegroundColor = UIColor.systemGreen
+        
+        twentyButton.configuration?.background.backgroundColor = UIColor.clear
+        twentyButton.configuration?.baseForegroundColor = UIColor.systemGreen
+        
+        zeroButton.configuration?.background.backgroundColor = UIColor.clear
+        zeroButton.configuration?.baseForegroundColor = UIColor.systemGreen
     }
     
     @IBAction func numberOfPersonStepper(_ sender: UIStepper) {
+        var choosedPersonNumber  = Int(sender.value)
+        ChoosedPersonLabel.text = String(choosedPersonNumber)
+        
+        inputTextField.endEditing(true)
     }
     
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
     }
     
+    
+    
+    @IBAction func dismiskeyboard(_ sender: UITapGestureRecognizer) {
+        inputTextField.endEditing(true)
+    }
 }
 
